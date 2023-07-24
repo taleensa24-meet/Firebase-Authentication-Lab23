421141d8244cf39bf33e567e4ed29ff1cd982cc8
taleensa24-meet <139681215+taleensa24-meet@users.noreply.github.com>
2023-07-24 17:51:58 +0300

    r

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -7,11 +7,12 @@ config={"apiKey": "AIzaSyB5KrNO6drGs2OryII5jnSPtpSmqlQJsyU",
   "projectId": "cs-lab-dd1b4",
   "storageBucket": "cs-lab-dd1b4.appspot.com",
   "messagingSenderId": "883753514061",
-  "appId": "1:883753514061:web:08729c7b58823c05a9d804", "databaseURL": ""  }
+  "appId": "1:883753514061:web:08729c7b58823c05a9d804", "databaseURL": "https://cs-lab-dd1b4-default-rtdb.europe-west1.firebasedatabase.app/"  }
 
 
 firebase=pyrebase.initialize_app(config)
 auth=firebase.auth()
+db=firebase.database()
 
 
 
@@ -39,15 +40,33 @@ def signup():
        password = request.form['password']
        try:
             login_session['user'] = auth.create_user_with_email_and_password(email, password)
+            UID=login_session['user']['localId']
+            user={"full_name":"full_name","username":"username","bio":"bio"}
+            db.child("Users").set(user)
             return redirect(url_for('add_tweet'))
        except Exception as e:
            error = "Authentication failed"
     return render_template("signup.html")
 
 
+
+
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
+    if request.method=="POST":
+        text= request.form["text"]
+        title= request.form["title"]
+        uid=login_session['user']['localId']
+        tweet={"title":title,"text":text,"uid":uid}
+        db.child("tweet").push(tweet)
+        return redirect(url_for("tweets"))
     return render_template("add_tweet.html")
+
+@app.route('/tweets', methods=['GET', 'POST'])
+def tweets():
+    tweets= db.child("tweet").get().val()
+    return render_template("tweets.html", tweets=tweets)
+
 
 
 if __name__ == '__main__':
